c17297b27caf16834329cd4175037d72c8a17ad6
nathaniel <letourneau.nathaniel@hotmail.com>
2026-01-28 20:41:12 -0500

    not really working, just committing in push to keep a record

--- flake.nix
+++ flake.nix
@@ -1,19 +1,34 @@
 {
-  description = "A simple NixOS flake";
-
-  inputs = {
-    # NixOS official package source, using the nixos-25.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  nixConfig = {
+    abort-on-warn = true;
+    allow-import-from-derivation = false;
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.grey-slate = nixpkgs.lib.nixosSystem {
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
-      ];
+  inputs.self.submodules = true;
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      inputs.import-tree [
+        ./hosts
+        ./modules
+      ]
+  );
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/25.11";
+
+    import-tree.url = "github:vic/import-tree";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+
+    #home-manager = {
+    #  url = "./inputs/home-manager";
+    #  flake = true;
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
   };
 }
