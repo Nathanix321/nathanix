bcd1ce07e5153170e8a164c05ca272adc32efa78
nathaniel <letourneau.nathaniel@hotmail.com>
2026-02-03 22:44:36 -0500

    I can't make sense of vic's config. I cloned quasigod's config, and it looks more approachable, so I'll try copying that.

--- modules/namespace.nix
+++
@@ -1,9 +0,0 @@
-{ inputs, den, ... }:
-{
-  _module.args.__findFile = den.lib.__findFile;
-  imports = [
-    (inputs.den.namespace "nathanix" true)
-    (inputs.den.namespace "nathaniel" false)
-    (inputs.den.namespace "my" false)
-  ];
-}
--- modules/non-dendritic.nix
+++
@@ -1,6 +0,0 @@
-{ den, ... }:
-{
-  den.default.includes = [
-    (den._.import-tree._.host ../nix/hosts)
-  ];
-}
